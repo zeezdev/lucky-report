794add7f0111ed8b4ff69cdb8ba8fce852ff0d24
user <user@local>
2018-02-16 09:41:29 -0400

    Get foreign keys from a table.

--- site/ln2sql/pg_database_helper.py
+++ site/ln2sql/pg_database_helper.py
@@ -46,15 +46,9 @@ class PgDatabaseHelper:
         return [row[0] for row in rows]
 
     def get_foreign_keys(self):
-        query = """SELECT
-                    tc.table_schema, tc.table_name, kcu.column_name,
-                    ccu.table_schema AS foreign_table_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name
-                FROM
-                    information_schema.table_constraints AS tc
-                    JOIN information_schema.key_column_usage AS kcu USING (constraint_name)
-                    JOIN information_schema.constraint_column_usage AS ccu USING (constraint_name)
-                WHERE
-                    tc.constraint_type = 'FOREIGN KEY';"""
+        query = """SELECT table_schema, table_name, column_name, 
+                foreign_table_schema, foreign_table_name, foreign_column_name
+                FROM foreign_keys;"""
         rows = self._get_rows(query)
         return [{'table_schema': row[0], 'table_name': row[1], 'column_name': row[2],
                  'foreign_table_schema': row[3], 'foreign_table_name': row[4], 'foreign_column_name': row[5]}
