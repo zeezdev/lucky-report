dd4dda3dbeb12d37641133c744de249cd1d0872f
user <user@local>
2018-02-16 02:43:35 -0400

    Make stop words unique.

--- site/ln2sql/stopwordFilter.py
+++ site/ln2sql/stopwordFilter.py
@@ -5,20 +5,20 @@ import unicodedata
 
 class StopwordFilter:
     def __init__(self):
-        self.list = []
+        self.words = set()
 
     def add_stopword(self, word):
-        self.list.append(word)
+        self.words.add(word)
 
     def get_stopword_list(self):
-        return self.list
+        return self.words
 
     def filter(self, sentence):
         tmp_sentence = ""
         words = re.findall(r"[\w]+", self.remove_accents(sentence))
         for word in words:
             word = self.remove_accents(word).lower()
-            if word not in self.list:
+            if word not in self.words:
                 tmp_sentence += word + " "
         return tmp_sentence.strip()
 
